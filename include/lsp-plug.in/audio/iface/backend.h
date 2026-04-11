df7d73816aff89e2203429babbc73431872a2afb
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-04-12 01:25:48 +0300

    Updated order of methods in a virtual table

--- include/lsp-plug.in/audio/iface/backend.h
+++ include/lsp-plug.in/audio/iface/backend.h
@@ -88,6 +88,15 @@ namespace lsp
             status_t        (* unregister_port)(backend_t *self, port_id_t port_id);
 
             /**
+             * Set latency for the data port.
+             * @param self pointer to backend_t structure
+             * @param port_id unique port identifier
+             * @param latency port latency in samples
+             * @return status of operation
+             */
+            status_t        (* set_port_latency)(backend_t *self, port_id_t port_id, uint32_t latency);
+
+            /**
              * Get system name of the port
              * @param self pointer to backend_t structure
              * @param port_id port identifier
@@ -114,15 +123,6 @@ namespace lsp
              * @return status of operation, STATUS_ALREADY_BOUND if already connected.
              */
             status_t        (* disconnect_ports)(backend_t *self, const char *source, const char *destination);
-
-            /**
-             * Set latency for the data port.
-             * @param self pointer to backend_t structure
-             * @param port_id unique port identifier
-             * @param latency port latency in samples
-             * @return status of operation
-             */
-            status_t        (* set_port_latency)(backend_t *self, port_id_t port_id, uint32_t latency);
 
             /**
              * Get number of audio buffers for the port. Input audio buffers may contain more than one

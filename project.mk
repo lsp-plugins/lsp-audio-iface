6182bb86fad8c82df1c6ab6ddc3300844a41de16
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-08-13 21:32:57 +0300

    Preparing for release

--- project.mk
+++ project.mk
@@ -23,4 +23,4 @@ ARTIFACT_ID                 = LSP_AUDIO_IFACE
 ARTIFACT_NAME               = lsp-audio-iface
 ARTIFACT_DESC               = Audio backend interface
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.1-devel
+ARTIFACT_VERSION            = 1.0.1
